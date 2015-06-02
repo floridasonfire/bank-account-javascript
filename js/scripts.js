124c6ba2e65f6badba3aab18d28a13d3fd64acba
Brent Bailey & Jessica Engel <student@epicodus.com>
2015-06-02 10:08:14 -0700

    holders able to deposit and withdraw money

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,14 @@ function bankAccount(holder, balance){
 };
 
 
+bankAccount.prototype.deposit = function(amount){
+  this.balance += amount
+};
+
+bankAccount.prototype.withdraw = function(amount){
+  this.balance -= amount
+};
+
 "use strict";
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -5,3 +5,18 @@ describe('bankAccount', function() {
     expect(testAccount.balance).to.equal(3000);
   });
 });
+describe('deposit', function(){
+  it("allows an account holder to deposit ca$h money", function() {
+    var testAccount = new bankAccount("Brent", 2000)
+    testAccount.deposit(5000)
+    expect(testAccount.balance).to.equal(7000)
+  });
+});
+
+describe('withdraw', function(){
+  it("allows an account holder to deposit ca$h money", function() {
+    var testAccount = new bankAccount("Jess", 10)
+    testAccount.withdraw(9)
+    expect(testAccount.balance).to.equal(1)
+  });
+});
