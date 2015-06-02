c7489d7bde7b5a9b03a031a360170af9f7371e34
Brent Bailey & Jessica Engel <student@epicodus.com>
2015-06-02 11:16:37 -0700

    have functioning frontend

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var btnId = ""
+
 function bankAccount(holder, balance){
   this.holder = holder
   this.balance = balance
@@ -13,11 +15,18 @@ bankAccount.prototype.withdraw = function(amount){
 };
 
 
+function findButton(id){
+    btnId = id
+    console.log(id)
+  };
+
+
 "use strict";
 
 
 //jQuery
 $( document ).ready(function() {
+
 
   $("#create_account").submit(function(event){
     event.preventDefault();
@@ -26,12 +35,30 @@ $( document ).ready(function() {
     var inputtedLastName = $("#last_name").val();
     var inputtedBalance = $("#balance").val();
     var newAccount = new bankAccount(inputtedFirstName + " " + inputtedLastName, inputtedBalance);
+
     $("#account_list").append("<li class='account'>" + newAccount.holder + "</li>");
+
     $(".account").last().click(function() {
       $("#show_account").show();
       $(".holder_name").text(newAccount.holder);
       $(".account_balance").text(newAccount.balance);
+
+
+      $("#change-balance").submit(function(event){
+        event.preventDefault();
+        var amt = parseInt($("#amt").val())
+        if (btnId === "withdraw") {
+          newAccount.withdraw(amt);
+          console.log(newAccount.balance)
+          console.log(btnId)
+        } else {
+          newAccount.deposit(amt);
+        };
+        $(".account_balance").text(newAccount.balance);
+      });
+
     });
+
 
 
   });
